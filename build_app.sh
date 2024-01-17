fdbac66a39137314e84893c61671c840aa14f74a
amari <amari.lawal@gmail.com>
2024-01-17 13:35:40 +0000

    anime no github workflow

--- build_app.sh
+++ build_app.sh
@@ -1,6 +1,6 @@
 git add .
 git commit -m "$1"
-git push orign -u main:main
+git push origin -u main:main
 docker build -t palondomus/amarianime:latest .
 docker push palondomus/amarianime:latest
 docker run -it -p 8080:8080 palondomus/amarianime:latest
