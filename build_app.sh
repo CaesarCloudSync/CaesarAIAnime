499850519be20ca4702465c5ed93633fbb261f41
amari <amari.lawal@gmail.com>
2024-02-11 15:17:30 +0000

    working with anime app branch

--- build_app.sh
+++ build_app.sh
@@ -1,6 +1,6 @@
 git add .
 git commit -m "$1"
 git push origin -u main:main
-docker build -t palondomus/amarianime:finest .
-docker push palondomus/amarianime:finest
-docker run -it -p 8080:8080 palondomus/amarianime:finest
+docker build -t palondomus/amarianime:coldest .
+docker push palondomus/amarianime:coldest
+docker run -it -p 8080:8080 palondomus/amarianime:coldest
